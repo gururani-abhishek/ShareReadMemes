7f39f132fdef4df1fe9a676d4213f99ec5427a5a
abhishekgururani <gururani.abhishek@gmail.com>
2021-07-16 19:31:22 +0530

    removed status bar

--- app/src/main/java/org/example/sharememes/MainActivity.kt
+++ app/src/main/java/org/example/sharememes/MainActivity.kt
@@ -2,17 +2,15 @@ package org.example.sharememes
 
 import android.content.Intent
 import android.graphics.drawable.Drawable
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -27,6 +25,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val decorView = window.decorView
+        val uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
+        decorView.systemUiVisibility = uiOptions
         loadMeme()
     }
     fun loadMeme() {
